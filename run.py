a871e5c9cebb55945b6fef33c8c9f696a6397e05
Thiagox789 <almadathiagoignacio@gmail.com>
2025-04-30 11:48:26 -0300

    Configuración inicial y conexión a la base de datos

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
 
+# Crear las tablas de la base de datos al iniciar la aplicación
+with app.app_context():
+    db.create_all()  # Esto crea todas las tablas definidas en los modelos
+
 if __name__ == "__main__":
     app.run(debug=True)
+
